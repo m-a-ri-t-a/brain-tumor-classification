2834bac86ba5a7f70d76bb095ccf13af6425ee76
Ante Drinovac <ante.drinovac@ultrax.ai>
2025-01-06 22:38:09 +0100

    Train VGG-16 model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models import resnet18, ResNet18_Weights, vgg16, VGG16_Weights
 import torch.nn.functional as F
 
 class MRIResNetClassifier(nn.Module):
@@ -16,5 +16,27 @@ class MRIResNetClassifier(nn.Module):
 
     def forward(self, x):
         x = self.resnet(x)
+        x = F.softmax(x, dim=1)
+        return x
+
+
+class MRI_VGG16_Classifier(nn.Module):
+    def __init__(self, num_classes, train_vgg16=False):
+        super(MRI_VGG16_Classifier, self).__init__()
+
+        # Load pre-trained VGG16 model
+        self.vgg16 = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+
+        for param in self.vgg16.parameters():
+            param.requires_grad = train_vgg16
+
+        in_features = self.vgg16.classifier[6].in_features
+        self.vgg16.classifier[6] = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        # Pass the input through the VGG16 network
+        x = self.vgg16(x)
+
+        # Apply softmax to get probability distribution
         x = F.softmax(x, dim=1)
         return x
