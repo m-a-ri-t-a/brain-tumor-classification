00a94a251f5e3fa308c410b60ba6defcdd624225
Marita Radić <marita.radic.mr@gmail.com>
2025-01-07 14:42:44 +0100

    Dodala pretrained param i kod za trenirati

--- models.py
+++ models.py
@@ -4,13 +4,15 @@ from torchvision.models import resnet18, ResNet18_Weights, vgg16, VGG16_Weights
 import torch.nn.functional as F
 
 class MRIResNetClassifier(nn.Module):
-    def __init__(self, num_classes, train_resnet = False):
+    def __init__(self, num_classes, pretrained=False, train_resnet = False):
         super(MRIResNetClassifier, self).__init__()
-        self.resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
-        
-        for param in self.resnet.parameters():
-            param.requires_grad = train_resnet
-        
+        if not pretrained:
+            self.resnet = resnet18()
+        else:
+            self.resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+            for param in self.resnet.parameters():
+                param.requires_grad = train_resnet
+
         in_features = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(in_features, num_classes)
 
@@ -21,22 +23,19 @@ class MRIResNetClassifier(nn.Module):
 
 
 class MRI_VGG16_Classifier(nn.Module):
-    def __init__(self, num_classes, train_vgg16=False):
+    def __init__(self, num_classes, pretrained=False, train_vgg16=False):
         super(MRI_VGG16_Classifier, self).__init__()
-
-        # Load pre-trained VGG16 model
-        self.vgg16 = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
-
-        for param in self.vgg16.parameters():
-            param.requires_grad = train_vgg16
-
+        if not pretrained:
+            self.vgg16 = vgg16()
+        else:
+            self.vgg16 = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+            for param in self.vgg16.parameters():
+                param.requires_grad = train_vgg16
+        
         in_features = self.vgg16.classifier[6].in_features
         self.vgg16.classifier[6] = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
-        # Pass the input through the VGG16 network
         x = self.vgg16(x)
-
-        # Apply softmax to get probability distribution
         x = F.softmax(x, dim=1)
         return x
